8edeff77202fdbaca21d7d2a846760c62e106599
iuhoay <iuhoay@gmail.com>
2013-06-01 09:25:14 +0800

    groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Groupmy::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root :to => 'high_voltage/pages#show', :id => 'welcome'
+
+  resources :groups
 end
