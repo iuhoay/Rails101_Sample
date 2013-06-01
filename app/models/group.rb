4eb9e16ee3c7ee01625a99bfe28ee2201ed47a92
iuhoay <iuhoay@gmail.com>
2013-06-01 20:09:47 +0800

    model group_user

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,8 @@ class Group < ActiveRecord::Base
 
   has_many :posts
   belongs_to :owner, class_name: "User", foreign_key: :user_id
+  has_many :group_users
+  has_many :participated_groups, through: :group_users, source: :group
 
   validates :title, presence: true
 
